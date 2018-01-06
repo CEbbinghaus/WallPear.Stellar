242460d3513b349177060237a177ba4ab9d55838
Unknown <jecebbi@gmail.com>
2018-01-06 23:47:21 +1100

    Updated sound controlls and sensetivity slider
    Just more functionallity

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ window.onload = function (){
             }
         })
     }
+    c.style.background = "#08303e"
 }
 
 //Declaring Variables
@@ -25,10 +26,39 @@ var A = toRadian(Math.random() * 360);
 var lastTime = 0;
 var DeltaTime = 0;
 var avg2;
+var D = 0;
+var loc = 3;
+var siz = 3;
+var sens = 1;
+var Color = [255, 255, 255]
 
 window.wallpaperPropertyListener = {
     applyUserProperties: function(properties) {
-
+        if(properties.bcol){
+            var background=properties.bcol.value.split(' ').map(function(c){return Math.ceil(c*255)});
+            c.style.background='rgb('+background+')'
+        }
+        if(properties.pcol){
+            Color = properties.pcol.value.split(' ').map(function(c){return Math.ceil(c*255)});
+        }
+        if(properties.draw){
+            D = properties.draw.value;
+        }
+        if(properties.rad){
+            rad = properties.rad.value;
+        }
+        if(properties.dis){
+            dis = properties.dis.value * 10;
+        }
+        if(properties.sens){
+            sens = properties.sens.value;
+        }
+        if(properties.loc){
+            loc = properties.loc.value;
+        }
+        if(properties.siz){
+            siz = properties.siz.value;
+        }
     }
 }
 
@@ -56,52 +86,50 @@ function wallpaperAudioListener(audioArray) {
         Average[i] = Math.max(Right[i], Left[i])
     }
 
-    avg2 = Average.slice(0, 6).reduce((a, b) => a + b, 0) / 6;
+    let low = loc - siz;
+    let high = loc + siz;
+    if(low < 0)low = 0;
+    if(high > Average.length - 1)high = Average.length - 1;
+    let am = high - low;
+    if(am == 0)am = 1;
+    console.log(low, high, am)
+
+    avg2 = Average.slice(low, high).reduce((a, b) => a + b, 0) / am * sens;
 
     ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-    ctx.fillStyle = "#08303e"
-    ctx.fillRect(0, 0, window.innerWidth, window.innerHeight)
-    ctx.fillStyle = "#FFFFFF"
-    ctx.strokeStyle = "#FFFFFF"
     //Update Each point to make it move
     points.forEach((p, i) => {
         let k = points[i];
-        k.x += k.v.x * avg2 //totalAmount //(Average[i] / 2);
-        k.y += k.v.y * avg2 //totalAmount //(Average[i] / 2);
-        /*
-        if(k.x < 0){
-            k.x += window.innerWidth;
-        }else{
-            k.x %= window.innerWidth;
-        }
-        if(k.y < 0){
-            k.y += window.innerHeight;
-        }else{
-            k.y %= window.innerHeight;
-        }*/
+        k.x += k.v.x * avg2 /*totalAmount*/ //(Average[i] / 2);
+        k.y += k.v.y * avg2 /*totalAmount*/ //(Average[i] / 2);
         if(k.x - rad <= 0){k.v.x = Math.abs(k.v.x);}
         if(k.x + rad >= window.innerWidth){k.v.x = -Math.abs(k.v.x);}
         if(k.y - rad <= 0){k.v.y = Math.abs(k.v.y);}
         if(k.y + rad >= window.innerHeight){k.v.y = -Math.abs(k.v.y);}
 
-        ctx.beginPath();
-        ctx.arc(p.x,p.y,rad * clamp(avg2, 0.6, 0.9),0,2*Math.PI);
-        ctx.fill();
-        points.forEach(p1 => {
-            //console.log(Math.sqrt(p.x*p1.x + p.y*p1.y))
-            if(Math.dist(p.x, p.y, p1.x, p1.y) < dis){
-                let a = 1 - Math.dist(p.x, p.y, p1.x, p1.y) / dis
-                //ctx.globalAlpha = a;
-                ctx.strokeStyle = "rgba(255, 255, 255, "+a+")"
-                ctx.beginPath();
-                ctx.moveTo(p.x, p.y);
-                ctx.lineTo(p1.x, p1.y);
-                ctx.stroke();
-                //ctx.globalAlpha = 1;
-            }
-        })
+        if(D == 0 || D == 2){
+            ctx.beginPath();
+            ctx.arc(p.x,p.y,rad * clamp(avg2, 0.6, 0.9),0,2*Math.PI);
+            ctx.fill();
+        }
+        if(D == 0 || D == 1){
+            points.forEach(p1 => {
+                if(p.x.around(p1.x, dis) && p.x.around(p1.x, dis)){
+                    let d = Math.dist(p.x, p.y, p1.x, p1.y);
+                    if(d < dis){
+                        let a = 1 - d / dis
+                        ctx.strokeStyle = `rgba(${Color},${a})`;
+                        ctx.fillStyle = `rgb(${Color})`;
+                        ctx.beginPath();
+                        ctx.moveTo(p.x, p.y);
+                        ctx.lineTo(p1.x, p1.y);
+                        ctx.stroke();
+                        ctx.globalAlpha = 1;
+                    }
+                }
+            })
+        }
     })
-
     let ct = Date.now();
     DeltaTime = ct - lastTime;
     lastTime = ct;
@@ -127,4 +155,12 @@ var clamp = function(k, mi, ma){
     if(k < mi)return mi;
     if(k > ma)return ma;
     return k;
+}
+
+Number.prototype.around = function(n, a){
+    if(n < this + a && n > this - a){
+        return true;
+    }else{
+        return false;
+    }
 }
